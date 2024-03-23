3199dbd282910927a4c2883417a233d8f56b509b
tuzov <roma_tuzov2016@mail.ru>
2024-03-23 20:17:41 +0300

    remove unused code in settingsVM

--- app/src/main/java/com/easyflow/diarycourse/features/feature_settings/SettingsViewModel.kt
+++ app/src/main/java/com/easyflow/diarycourse/features/feature_settings/SettingsViewModel.kt
@@ -2,66 +2,11 @@ package com.easyflow.diarycourse.features.feature_settings
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
 import com.easyflow.diarycourse.core.BaseViewModel
-import com.easyflow.diarycourse.domain.domain_api.NoteUseCase
-import com.easyflow.diarycourse.domain.domain_api.ScheduleUseCase
 import com.easyflow.diarycourse.features.feature_calendar.models.CombineModel
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class SettingsViewModel @Inject constructor(
-    private val scheduleUseCase: ScheduleUseCase,
-    private val noteUseCase: NoteUseCase
-) : BaseViewModel<SettingsViewModel.State, SettingsViewModel.Actions>(SettingsViewModel.State()) {
-
-    private fun fetchData() {
-        viewModelScope.launch {
-            val combineModels: MutableList<CombineModel> = mutableListOf()
-
-            coroutineScope {
-                val scheduleItemsDeferred = async { scheduleUseCase.getAll() }
-                val noteItemsDeferred = async { noteUseCase.getAll() }
-
-                // Ждем выполнения обеих корутин
-                val scheduleItems = scheduleItemsDeferred.await()
-                val noteItems = noteItemsDeferred.await()
-
-                // Создаем список CombineModel и добавляем в него элементы из обеих списков
-                combineModels.addAll(
-                    scheduleItems.map { scheduleItem ->
-                        CombineModel(
-                            id = scheduleItem.id,
-                            text = scheduleItem.text,
-                            description = scheduleItem.description,
-                            date = scheduleItem.date,
-                            startTime = scheduleItem.startTime,
-                            endTime = scheduleItem.endTime,
-                            duration = scheduleItem.duration,
-                            color = scheduleItem.color,
-                            isCompleteTask = scheduleItem.isCompleteTask,
-                            priority = scheduleItem.priority
-                        )
-                    }
-                )
-                combineModels.addAll(
-                    noteItems.map { noteItem ->
-                        CombineModel(
-                            id = noteItem.id,
-                            text = noteItem.text,
-                            date = noteItem.date
-                        )
-                    }
-                )
-            }
-
-            if (combineModels.isNotEmpty()) {
-                modifyState { copy(list = combineModels) }
-            }
-        }
-    }
+class SettingsViewModel @Inject constructor() : BaseViewModel<SettingsViewModel.State, SettingsViewModel.Actions>(SettingsViewModel.State()) {
 
     data class State(
         var list: List<CombineModel> = emptyList()
@@ -71,13 +16,9 @@ class SettingsViewModel @Inject constructor(
         data class ShowAlert(val alertData: String) : Actions
     }
 
-    class SettingsViewModelFactory @Inject constructor(
-        private val scheduleUseCase: ScheduleUseCase,
-        private val noteUseCase: NoteUseCase
-    ) : ViewModelProvider.Factory {
-
+    class SettingsViewModelFactory @Inject constructor() : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return SettingsViewModel(scheduleUseCase, noteUseCase) as T
+            return SettingsViewModel() as T
         }
     }
 }
